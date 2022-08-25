26f7c4657487ec362e4eeb104065c913da52e339
Lineles <106559385+Lineles@users.noreply.github.com>
2022-08-25 16:04:21 +0200

    finish header

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -2,10 +2,23 @@ import "./Header.css"
 
 function Header (){
     return (
+        
         <div className="header-div">
-            <h3 className="header-h3"> The </h3>
-            <h1 className="header-h1"> Rick and Morty </h1>
-            <h3 className="header-h3-2"> Character Encyclopaedia</h3>
+             <div>
+                
+
+             </div>
+
+             <div >
+                 <h3 className="header-h3"> The </h3>
+                 <h1 className="header-h1"> Rick and Morty </h1>
+                 <h3 className="header-h3-2"> Character Encyclopaedia</h3>
+             </div>
+
+             <div>
+                
+            
+             </div>
         </div>
 ); 
 }
