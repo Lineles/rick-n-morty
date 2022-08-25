8b18282fff92f0eae29c246e134a4331a5a12d8e
Lineles <106559385+Lineles@users.noreply.github.com>
2022-08-25 16:15:26 +0200

    created basic Navbar.js & navbar.css

---
+++ src/Components/Navigation/Navigation.js
@@ -0,0 +1,24 @@
+import "./Navigation.css"
+
+function Navigation () {
+
+        return (
+
+            <nav> 
+                <div>
+
+                </div>
+                <div>
+                    <input type="text" placeholder="Search for Caracter" /> 
+                    <button> </button>
+                </div>
+                <div>
+
+                </div>
+            </nav>
+        );
+
+
+}
+
+export default Navigation; 
