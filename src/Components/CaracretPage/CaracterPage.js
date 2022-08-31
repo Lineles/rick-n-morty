5f2c64ccf17febe4d7808337227e3cb72b063ac7
Lineles <106559385+Lineles@users.noreply.github.com>
2022-08-31 14:14:14 +0200

    design info section

--- src/Components/CaracretPage/CaracterPage.js
+++ src/Components/CaracretPage/CaracterPage.js
@@ -24,38 +24,22 @@ function CaracterPage () {
    
     
     return (
-    <div className="MainDiv">
-      <img src="\images\collection-torn-paper-png.png" alt="papier" className="papier"></img>
+      <div className="MainDiv">
+      
+      <div className="imageDIV">
+        <img src={image} alt={name} className="caracter-img"/>
+      </div>
 
-      <div className="info-flex">
-        <div className="imageDIV">
-          <img src={image} alt={name} className=""/>
+      <div className="infocard">
+        <img src="\images\collection-torn-paper-png.png" alt="papier" className="papier"></img>
+        <div className="info">
+          <div className="lineflex"> <p className="infos">Status:</p> <p className="answers">{status}</p> </div> 
+          <div className="lineflex"> <p className="infos">Species:</p>   <p className="answers">{species}</p></div>
+          <div className="lineflex"> <p className="infos">Type:</p>   <p className="answers">{type}</p></div>
+          <div className="lineflex"> <p className="infos">Gender:</p>    <p className="answers">{gender}</p></div>
+          <div className="lineflex"> <p className="infos">Last known Location:</p>  <p className="answers">{location?.name}</p> </div>
+          <div className="lineflex"> <p className="infos">Origin:</p> <p className="answers">{origin?.name}</p> </div>
         </div>
-        <div className="basicInfo">
-            <div className="nameDIV">
-              {name}
-            </div>
-            <div className="info"> 
-              <div className="statusDIV">
-                Status: {status}
-              </div>
-              <div className="speciesDIV">
-                Species: {species}
-              </div>
-              <div className="typeDIV">
-                Type: {type}
-              </div>          
-              <div className="genderDIV">
-                Gender: {gender}
-              </div>
-              <div className="locationDIV">
-                Last known Location: {location?.name}
-              </div>
-              <div className="originDIV">
-                Origin: {origin?.name}
-              </div>
-            </div>   
-        </div>       
       </div>
     </div>
     )
