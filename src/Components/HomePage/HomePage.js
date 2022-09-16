640bfd7bda4b1b5cb17358e96a4be9ca6b543383
Lineles <106559385+Lineles@users.noreply.github.com>
2022-09-16 21:24:55 +0200

    sicherheits commit

--- src/Components/HomePage/HomePage.js
+++ src/Components/HomePage/HomePage.js
@@ -19,39 +19,10 @@ function HomePage () {
                     <ChatPanel /> 
                     <CaractersPanel />
 
-               
 
-                    <div class="homepage-panel4">
-                        <img  className="panel4-img" src="\images\birdperson.png" alt="Bird Preson" />
-                        <div class="overlay4">
-                            <img  className="panel4-img2" src="\images\phenixperson.PNG" alt="Bird Preson" />
-                            <img  className="panel4-img3" src="\images\abradolflincon.png" alt="Abradolf" />
-                        </div>
-                    </div>
-                
-                    <div class="homepage-panel5">
-                        <h1 className="panel5-h1"> Chat corner</h1>
-                        <img  className="panel5-img" src="\images\birdperson.png" alt="Bird Preson" />
-                        <div class="overlay5">
-                            <div class="overlay5-div">
-                                <h1 className="panel5-h1-2"> Chat corner</h1>
-                            </div>
-                            <img  className="panel5-img2" src="\images\phenixperson.PNG" alt="Bird Preson" />
-                        </div>
-                    </div>
-                    <div class="homepage-panel6">
-                        <img  className="panel6-img" src="\images\birdperson.png" alt="Bird Preson" />
-                        <div class="overlay6">
-                            <img  className="panel6-img2" src="\images\phenixperson.PNG" alt="Bird Preson" />
-                        </div>
-                    </div>
-            
                 </div> 
                 
             
-
-
-            <h1>Hello</h1>
         </div>
     )
 
--- src/Components/HomePage/Panels/CaractersPanel.js
+++ src/Components/HomePage/Panels/CaractersPanel.js
@@ -4,9 +4,13 @@ function CaractersPanel () {
 
 return(
 
-    <div >
-        
-    </div>
+        <div class="homepage-CaractersPanel">
+            <img  className="CaractersPanel-img" src="\images\birdperson.png" alt="Bird Preson" />
+            <div class="CaractersPanel-overlay">
+                <img  className="CaractersPanel-overlay-img1" src="\images\phenixperson.PNG" alt="Bird Preson" />
+                {/* <img  className="CaractersPanel-overlay-img2" src="\images\abradolflincon.png" alt="Abradolf" /> */}
+            </div>
+        </div>
 
 );
 };
