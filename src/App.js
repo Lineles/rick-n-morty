6c58adcfa7ce751eae1531f3ab17d30c3048b468
Lineles <106559385+Lineles@users.noreply.github.com>
2022-09-19 11:21:55 +0200

    changes in App.js

--- src/App.js
+++ src/App.js
@@ -33,9 +33,13 @@ function App() {
     <Router>
       <Header /> 
       <NavigationNoSerch />
-        <Routes>
-          <Route path="/homepage" element={<HomePage results={results}/> } /> 
-          <Route path="/" element={<CaracterCard page="/" results={results} setpage={setpage} setSearch={setSearch} currentPage={page} />}/>
+        <Routes> 
+          <Route path="/" element={<HomePage page="/" results={results} setpage={setpage} setSearch={setSearch} currentPage={page}/> } /> 
+          <Route path="/homepage" element={<CaracterCard page="/homepage" results={results} setpage={setpage} setSearch={setSearch} currentPage={page} />}/>
+
+          {/* <Route path="/homepage" element={<HomePage results={results}/> } /> 
+          <Route path="/" element={<CaracterCard page="/" results={results} setpage={setpage} setSearch={setSearch} currentPage={page} />}/> */}
+          
           <Route path="/:id" element={<CaracterPage  results={results} />}/>
           <Route path="/registration" element={<Registration /> }/>
           <Route path="/yourCaracter" element={<YourCaracter />} /> 
@@ -46,3 +50,7 @@ function App() {
 }
 
 export default App;
+
+
+
+         
